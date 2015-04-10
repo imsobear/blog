3a6b43acc1c02e1cc5a7075c2d4c5793c9a0ebfe
imsobear <sobear.me@gmail.com>
2015-04-10 11:44:07 +0800

    let me try try

---
+++ _book/index.php
@@ -0,0 +1,15 @@
+<!-- 跳转一个，index.html 会使 gitbook 抽风 -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>果哥的三分地</title>
+
+	<meta http-equiv="refresh" content="0; url=./_book/index.html" />
+
+</head>
+<body>
+
+</body>
+</html>
